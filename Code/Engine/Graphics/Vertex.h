cb85413b004af1c94510f8c85058a5ab6706fa03
Jon Kenkel <jonkenkel@outlook.com>
2016-02-08 16:01:49 -0700

    Graphics now has D3D Debugging enabled for x64 builds in debug mode.

--- Code/Engine/Graphics/Vertex.h
+++ Code/Engine/Graphics/Vertex.h
@@ -21,7 +21,7 @@ namespace Lame
 	public:
 		Lame::Vector3 position;		// POSITION, 3 floats == 12 bytes, Offset = 0
 		Lame::Vector2 texcoord;		// TEXCOORD0, 2 floats = 8 bytes, Offset 12
-		Color32 color;					// COLOR0, 4 uint8_ts == 4 bytes, Offset = 20
+		Color32 color;				// COLOR0, 4 uint8_ts == 4 bytes, Offset = 20
 
 		Vertex() {}
 		Vertex(Lame::Vector3 i_pos, Lame::Vector2 i_tex, Color32 i_color) : position(i_pos), texcoord(i_tex), color(i_color) {}
